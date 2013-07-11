54116a3884dd3cc04bcebd820fb311f918ccfbb1
raman <raman@fbk.eu>
2013-07-11 10:08:08 +0200

    Temporary mappings for parking names

--- src/main/java/eu/trentorise/smartcampus/domain/smartplannerdata/CarParkingDataConverter.java
+++ src/main/java/eu/trentorise/smartcampus/domain/smartplannerdata/CarParkingDataConverter.java
@@ -22,6 +22,7 @@ import java.io.Serializable;
 import java.math.BigInteger;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -36,6 +37,29 @@ import eu.trentorise.smartcampus.domain.discovertrento.POIData;
 import eu.trentorise.smartcampus.services.smartplannerdata.data.message.Smartplannerdata.CarParkingStation;
 
 public class CarParkingDataConverter implements DataConverter {
+
+	// TODO move the real IDs into the smartplanner directly
+	private static final Map<String, String> parkingsNames;
+	static {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("Circonvallazione Nuova, Ponte S. Lorenzo - Trento", "Parcheggio vecchia tangenziale di Piedicastello");
+		map.put("P1", "Parcheggio area ex SIT via Canestrini");
+		map.put("P2", "Garage Centro Europa");
+		map.put("P3", "Garage Autosilo Buonconsiglio");
+		map.put("P4", "Garage piazza Fiera");
+		map.put("P5", "Garage Parcheggio Duomo");
+		map.put("P6", "Parcheggio CTE via Bomporto");
+		map.put("P7", "Parcheggio piazzale Sanseverino");
+		map.put("ex-Zuffo - Trento", "Parcheggio Area ex Zuffo");
+		map.put("via Asiago, Stazione FS Villazzano - Trento", "Parcheggio via Asiago - Villazzano STAZIONE FS");
+		map.put("via Fersina - Trento", "Parcheggio Ghiaie via Fersina");
+		map.put("via Maccani - Trento", "Parcheggio Campo Coni via E. Maccani");
+		map.put("via Roggia Grande,16 - Trento", "Garage Autorimessa Europa");
+		map.put("via Roggia Grande,16-Trento", "Garage Autorimessa Europa");
+		map.put("via Torre Verde, 40 - Trento", "Garage Torre Verde");
+		map.put("via valentina Zambra - Trento", "Garage Parcheggio Palazzo Onda");
+		parkingsNames = Collections.unmodifiableMap(map);
+	}
 
 	@Override
 	public Serializable toMessage(Map<String, Object> parameters) {
@@ -65,8 +89,9 @@ public class CarParkingDataConverter implements DataConverter {
 	private GenericPOI extractGenericPOI(CarParkingStation s)
 			throws ParseException {
 		GenericPOI ge = new GenericPOI();
-		ge.setTitle("Parking "+s.getId());
-		ge.setDescription(createDescription(s));
+		String mappedId = parkingsNames.containsKey(s.getId()) ? parkingsNames.get(s.getId()) : ("Parcheggio "+s.getId());
+		ge.setTitle(mappedId);
+		ge.setDescription(createDescription(s, mappedId));
 		ge.setSource("smartplanner-parking");
 		ge.setPoiData(createPOIData(s));
 		ge.setType("Parking");
@@ -86,8 +111,8 @@ public class CarParkingDataConverter implements DataConverter {
 		return ge;
 	}
 
-	private String createDescription(CarParkingStation s) {
-		String d = "Parking "+s.getId()+".";
+	private String createDescription(CarParkingStation s, String mappedId) {
+		String d = mappedId+".";
 		if (s.hasAvailablePlaces()) d += " "+s.getAvailablePlaces()+" places.";
 		return d;
 	}
